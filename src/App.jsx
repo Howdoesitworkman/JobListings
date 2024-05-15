4cfd21bb411c82e28d0596e75e2a1d1a077cc525
Peng Wang <p.wang.1121@gmail.com>
2024-05-15 16:59:56 -0500

    add the job page with a data loader instead of use effect

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
 import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
-import JobPage from './pages/JobPage'
+import JobPage, {jobLoader} from './pages/JobPage'
 
 import {
   Route,
@@ -17,7 +17,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout />}>                {/*with this, all pages closed by '<Route path...>' share the MainLayout*/}
       <Route index element={<HomePage />} />                 {/*the 'index' can be replaced by any path we need*/}
       <Route path='/jobs' element={<JobsPage />}/>           {/*one page, one router */}
-      <Route path='/jobs/:id' element={<JobPage />}/>        {/*':' means the 'id' is dynamic */}
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader}/>        {/*':' means the 'id' is dynamic */}
       <Route path='*' element={<NotFoundPage />} />          {/*The '*' will cover all pages that we don't define and then show 404 */}
     </Route>
   )                           
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,32 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import Spinner from '../components/Spinner'
+import React from 'react'
+import { useParams, useLoaderData} from 'react-router-dom'
 
 //here we can also use 'use effect' to fetch data but we choose to use a data loader
 const JobPage = () => {
     const {id} = useParams()        // this is how we get the id of a job
-    const [job, setJob] = useState(null)
-    const [loading, setLoading] = useState(true)
+    const job = useLoaderData()
 
-    useEffect(()=>{
-        const fetchJob = async ()=>{
-            try{
-                const res = await fetch(`/api/jobs/${id}`)           // await is to wait for a Promise and get its fulillment value
-                const data = await res.json()
-                setJob(data)                             // after this line, 'jobs' is the same as 'data'
-            }catch(error){
-                console.log('Error fetching data', error)
-            }finally{
-                setLoading(false)                         // after this line, loadind becomes false
-            }
-        }
-        fetchJob()
-
-    }, [])
-
-    return loading ? <Spinner /> :(
-        <h1>{job.title}</h1>
-    )
+    return <h1>{job.title}</h1>
 }
 
-export default JobPage
+const jobLoader = async({params})=>{
+    const res = await fetch(`/api/jobs/${params.id}`)
+    const data = await res.json()
+    return data
+}
+
+
+export {JobPage as default, jobLoader}
