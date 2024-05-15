7dd0c5fe9ace8f999b8e9e9c9d8c6f73dfb1c299
Peng Wang <p.wang.1121@gmail.com>
2024-05-14 22:39:06 -0500

    add a Spinner and make HomePage show only 3 jobs

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useState, useEffect} from 'react'           // we need a state hook to create a side effect and fetch data by that
 import JobListing from './JobListing'
+import Spinner from './Spinner'
 
 const JobListings = ({isHome = false}) => {         // here it means the variable 'isHome' is false by default
     const [jobs, setJobs] = useState([]);           // by default this is empty. When the component loads, we will make a request and fill the array
@@ -8,8 +9,9 @@ const JobListings = ({isHome = false}) => {         // here it means the variabl
 
     useEffect(()=> {
         const fetchJobs = async () => {             // async makes a function return a Promise
+            const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3' : 'http://localhost:8000/jobs'     // if it is HomePage, only show 3 jobs
             try{
-                const res = await fetch('http://localhost:8000/jobs')           // await is to wait for a Promise and get its fulillment value
+                const res = await fetch(apiUrl)           // await is to wait for a Promise and get its fulillment value
                 const data = await res.json()
                 setJobs(data)                       // after this line, 'jobs' is the same as 'data'
             }catch(error){
@@ -26,16 +28,19 @@ const JobListings = ({isHome = false}) => {         // here it means the variabl
     <section className="bg-blue-50 px-4 py-10">
         <div className="container-xl lg:container m-auto">
             <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-            {isHome ? 'Recent Jobs' : 'All Jobs'}
+                {isHome ? 'Recent Jobs' : 'All Jobs'}
             </h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {loading ? (<h2>Loading...</h2>) : (
-                jobs.map((job)=>(
-                    <JobListing key={job.id} job={job} />
-                ))
+            {loading ? (
+                <Spinner loading={loading}/>
+            ) : (
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {
+                    jobs.map((job)=>(
+                        <JobListing key={job.id} job={job} />
+                    ))
+                }
+                </div>
             )}
-
-            </div>
         </div>
         </section>
     )
